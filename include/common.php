3521efe006da8d3dbb7e972151218bd6301b686e
opai8 <26339512+opai8@users.noreply.github.com>
2025-11-11 07:02:12 +0800

    Add files via upload

---
+++ include/common.php
@@ -0,0 +1,46 @@
+<?php
+
+@header("Content-type:text/html;charset=utf-8");
+define('IN_CRONLITE', true);
+define('SYS_KEY', 'lylme_key');
+define('SYSTEM_ROOT', dirname(__FILE__) . '/');
+define('ROOT', dirname(SYSTEM_ROOT) . '/');
+error_reporting(0);
+require ROOT . 'config.php';
+if(!defined('SQLITE') && (!$dbconfig['user'] || !$dbconfig['pwd'] || !$dbconfig['dbname'])) {
+    @unlink(ROOT . 'install/install.lock');
+    header("Location:");
+    exit();
+}
+require SYSTEM_ROOT . "db.class.php";
+$DB = new DB($dbconfig['host'], $dbconfig['user'], $dbconfig['pwd'], $dbconfig['dbname'], $dbconfig['port']);
+
+$web_config = $DB->query("SELECT * FROM `lylme_config`");
+if(empty($web_config)) {
+    exit("<h3>LyLme Spage Error: MySQL config table is empty(code:404)<h3>");
+}
+while($row = $DB->fetch($web_config)) {
+    $conf[$row['k']] = $row['v'];
+}
+
+require SYSTEM_ROOT . "lists.php";
+require SYSTEM_ROOT . "function.php";
+require SYSTEM_ROOT . "member.php";
+require SYSTEM_ROOT . "tj.php";
+require SYSTEM_ROOT . "version.php";
+require SYSTEM_ROOT . "updbase.php";
+require SYSTEM_ROOT . "site.php";
+$cdnpublic = cdnpublic($conf['cdnpublic']);
+$templatepath = './template/' . $conf["template"];
+$template =  $templatepath . '/index.php';
+$background = $conf["background"];
+$wap_background = $conf["wap_background"];
+if(checkmobile()) {
+    if(!empty($wap_background)) {
+        $background_img = $wap_background;
+    } else {
+        $background_img = $background;
+    }
+} else {
+    $background_img = $background;
+}
---
+++ install/data/update2.sql
@@ -0,0 +1,3 @@
+INSERT INTO `lylme_config` (`k`, `v`, `description`) VALUES
+('apply_gg', '<b>收录说明：</b><br>1. 禁止提交钓鱼弹窗站点<br>2. 申请收录请先添加本站友链<br>3. 填写网址后，可点击-自动获取-网站图标<br>4. 提交后请耐心等待,可加电报群了解申请进度<br>', '收录公告'),
+('wap_background', '/assets/img/wapbackground.jpg', '手机端背景');
